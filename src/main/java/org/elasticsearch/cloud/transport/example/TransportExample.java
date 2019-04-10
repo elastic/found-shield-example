d662197ff8b8dd5d80ba7253b57702fccbc12b4b
Igor Kupczyński <igor@kupczynski.info>
2019-04-10 17:53:59 +0200

    Update elasticsearch client dependency to 6.7
    Note that the ESLoggerFactory was removed in elasticsearch#35146

--- src/main/java/org/elasticsearch/cloud/transport/example/TransportExample.java
+++ src/main/java/org/elasticsearch/cloud/transport/example/TransportExample.java
@@ -19,12 +19,12 @@
 
 package org.elasticsearch.cloud.transport.example;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.elasticsearch.action.ActionFuture;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.logging.ESLoggerFactory;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
@@ -37,7 +37,7 @@ import java.util.concurrent.TimeUnit;
 
 public class TransportExample {
 
-    public Logger logger = ESLoggerFactory.getLogger(getClass().getCanonicalName());
+    public Logger logger = LogManager.getLogger(getClass());
     // Note: If enabling IPv6, then you should ensure that your host and network can route it to the Cloud endpoint.
     // (eg Docker disables IPv6 routing by default) - see also the system property parsing code below.
     private boolean ip6Enabled = true;
